b8ca410906ef7977218305df97b6c103cfad6363
Brendan Hickey <brendan@bhickey.net>
2012-07-13 17:14:12 -0700

    Have a glass of enums.

---
+++ src/enums.h
@@ -0,0 +1,7 @@
+#ifndef __TGSOH_ENUMS_HEADER__
+#define __TGSOH_ENUMS_HEADER__
+
+enum Moves { LEFT, RIGHT, UP, DOWN, WAIT, ABORT };
+enum Terrain { AIR, EARTH, EXIT, LAMBDA, ROBOT, ROCK, WALL };
+
+#endif /* __TGSOH_ENUMS_HEADER__ */
